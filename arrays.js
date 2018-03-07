de293a814bd2361f82df682f85b67839f3d84747
Brad Newman <newmanbradm@gmail.com>
2018-03-07 23:03:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,11 +17,14 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  array.shift()
+  return array
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
+  return array
 }
+
