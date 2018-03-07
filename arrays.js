65e75c369c6a3083b701944f7b71620d0659fa15
Brad Newman <newmanbradm@gmail.com>
2018-03-07 22:54:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+function destructiveleyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
